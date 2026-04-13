a116bb441bfab08c6bf7b93b1dafe505dbb4b708
n0ut1t <ikerblasor@gmail.com>
2026-04-13 20:21:39 +0200

    developed and added some imgs

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-
 const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/kroma'); 
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.get('/singup', (req,res) => {
+	res.render('sing_in');
+})
+
+app.get('/register', (req,res) => {
+	res.render('register')
+})
 
 app.get('/', (req,res) => {
-	res.render('sing_in');
+	res.redirect('/singup');
 });
 
 app.post('/mapa', (req,res) => {
---
+++ models/users.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, 'El nombre es obligatorio']
+    },
+    surname: {
+      type: String,
+      required: [false],
+    },
+    mail: {
+      type: String,
+      required: [true, 'El mail es obligatorio'],
+      match: [/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, 'El mail no es válido']
+    },
+    username: {
+      type: String
+    },
+    password: {
+      type: String,
+      required: [true, 'La contraseña es obligatoria'],
+      minlength: [8, 'La contraseña debe tener al menos 8 caracteres'],
+      select: false
+    }
+});
+
+const User = mongoose.model('users', contactoSchema);
+
+module.exports = User;

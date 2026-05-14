ea3325ba2888a80a90c51b0791bd9904086943fc
n0ut1t <ikerblasor@gmail.com>
2026-05-14 16:01:19 +0200

    antes de intentar poner el editor real

--- controllers/mapa.js
+++ controllers/mapa.js
@@ -4,3 +4,8 @@ exports.global = ('/',async (req, res) => {
   if (!res.locals.user) return res.redirect('/singin');
   res.render('mapa'); 
 });
+
+exports.niveles = ('/nivel/:id', async (req,res) => {
+  if (!res.locals.user) return res.redirect('/singin');
+  res.render('niveles');
+})
--- routes/mapa.js
+++ routes/mapa.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mapaController = require('../controllers/mapa');
 
 router.get('/', mapaController.global);
+router.get('/nivel/:id', mapaController.niveles)
 
 module.exports = router;
